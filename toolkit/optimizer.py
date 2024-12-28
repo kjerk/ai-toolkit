937edc624c498c1ac4f839ff870d7b95175dac26
kjerk <kjerkdev@gmail.com>
2024-12-27 21:59:37 -0800

    Add cmars, claprop, and cadamw variants. Add AdamW-C impl from paper repo.

--- toolkit/optimizer.py
+++ toolkit/optimizer.py
@@ -98,6 +98,16 @@ def get_optimizer(
     elif lower_type == 'automagic':
         from toolkit.optimizers.automagic import Automagic
         optimizer = Automagic(params, lr=float(learning_rate), **optimizer_params)
+    elif lower_type == 'cmars':
+        import timm.optim
+        optimizer = timm.optim.Mars(params, lr=float(learning_rate), caution=True, **optimizer_params)
+    elif lower_type == 'claprop':
+        import timm.optim
+        optimizer = timm.optim.LaProp(params, lr=float(learning_rate), caution=True, **optimizer_params)
+    elif lower_type == 'cadamw':
+        from toolkit.optimizers.c_adamw import CAdamW
+        optimizer = CAdamW(params, lr=float(learning_rate), **optimizer_params)
     else:
         raise ValueError(f'Unknown optimizer type {optimizer_type}')
+    
     return optimizer
